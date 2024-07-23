df4df1b15cdad76563a689cf3e1b273121813ed1
oluwamuibiclear <igbokweudoka@gmail.com>
2024-07-22 21:24:43 -0500

    add gradeApp

---
+++ gradeApp.js
@@ -0,0 +1,48 @@
+function getAverage(scores) {
+  // console.log(scores)
+const count = scores.length;
+let sum = 0;
+scores.map((element)=>{
+  sum+=element
+  // console.log(element)
+})
+return sum/count
+// console.log(sum)
+}
+
+console.log(getAverage([92, 88, 12, 77, 57, 100, 67, 38, 97, 89]));
+console.log(getAverage([45, 87, 98, 100, 86, 94, 67, 88, 94, 95]));
+
+function getAverage(scores) {
+    let sum = 0;
+  
+    for (const score of scores) {
+      sum += score;
+    }
+  
+    return sum / scores.length;
+  }
+  function getGrade(score) {
+    if(score <= 59){
+      return "F"
+    }
+    else if(score <= 69 && score > 59){
+      return "D"
+    }
+    else if(score <= 79 && score > 69){
+      return "C"
+    }
+    else if(score <= 89 && score > 79){
+     return  "B"
+    }
+    else if(score <= 99 && score > 89){
+      return "A"
+    }
+    else{
+  return "A++"
+    }
+  }
+  
+  console.log(getGrade(96));
+  console.log(getGrade(82));
+  console.log(getGrade(56));
--- pyramid-generator.js
+++ pyramid-generator.js
@@ -1,28 +1,29 @@
 const character = "#";
 const count = 8;
 const rows = [];
-let inverted = true;
+let inverted = false;
 
 function padRow(rowNumber, rowCount) {
   return " ".repeat(rowCount - rowNumber) + character.repeat(2 * rowNumber - 1) + " ".repeat(rowCount - rowNumber);
 }
 
 // TODO: use a different type of loop
-for (let i = 1; i <= count; i++) {
-  if (inverted) {
-    rows.unshift(padRow(i, count));
-  } else {
-    rows.push(padRow(i, count));
-  }
+// for (let i = 1; i <= count; i++) {
+//   if (inverted) {
+//     rows.unshift(padRow(i, count));
+//   } else {
+//     rows.push(padRow(i, count));
+//   }
+// }
+
+while (rows.length < count) {
+    if (inverted) {
+            rows.unshift(padRow(rows.length + 1, count));
+          } else {
+            rows.push(padRow(rows.length + 1, count));
+          }
+//   rows.push(padRow(rows.length + 1, count));
 }
-
-/*while (rows.length < count) {
-  rows.push(padRow(rows.length + 1, count));
-}*/
-
-/*for (let i = count; i > 0; i--) {
-  rows.push(padRow(i, count));
-}*/
 
 let result = ""
 
