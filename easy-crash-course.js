6517e1b9fa8456bc1f2402780618c7e10bd47ded
oluwamuibiclear <igbokweudoka@gmail.com>
2024-07-23 22:00:49 -0500

    sort objects

--- easy-crash-course.js
+++ easy-crash-course.js
@@ -10,17 +10,21 @@ function showRating(rating){
 return full_star;
 }
 function sortLowToHigh(data){
-    let i = 0
+    // let i = 0
     let full_star =data.sort((a,b)=>{
-        i++
-        console.log(`is ${a} greater than ${b}? ${a>b} return ${a>b?b:a}`)
-console.log(i+' =>'+a,b)
+        // i++
+//         console.log(`is ${a} greater than ${b}? ${a>b} return ${a>b?b:a}`)
+// console.log(i+' =>'+a,b)
 return a-b
     })
     return full_star
 }
 
-
-
+function sortObjectLowToHigh(data){
+    
+    let full_star =data.sort((a,b)=> a.price-b.price )
+    return full_star
+}
+console.log(sortObjectLowToHigh([{id:1,price:10},{id:2,price:5},{id:3,price:25}]));
 console.log(sortLowToHigh([5,7373,933,1781,393,474]));
 console.log(showRating(5));
