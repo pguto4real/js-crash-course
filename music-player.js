88205baf2e445aff91d14d6be80230d75c322a9f
oluwamuibiclear <igbokweudoka@gmail.com>
2024-07-29 00:08:13 -0500

    music player create sort songs function done

--- music-player.js
+++ music-player.js
@@ -102,6 +102,20 @@ const renderSongs = (array) => {
         }).join('')
     playlistSongs.innerHTML = songsHTML
 };
-renderSongs(userData?.songs)
 
-const sortSongs=()=>{}
+const sortSongs = () => {
+    userData?.songs.sort((a,b) => {
+      if (a.title < b.title) {
+        return -1;
+      }
+  
+      if (a.title > b.title) {
+        return 1;
+      }
+  
+      return 0;
+    });
+  
+    return userData?.songs;
+  };
+renderSongs(sortSongs());
