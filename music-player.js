dfccd76f5940aafe8ff438213cebbf757fa39a59
oluwamuibiclear <igbokweudoka@gmail.com>
2024-07-29 00:44:03 -0500

    music player create next song

--- music-player.js
+++ music-player.js
@@ -108,6 +108,17 @@ const pauseSong = () => {
   audio.pause();
 };
 
+const playNextSong = () => {
+  if (userData?.currentSong === null) {
+    playSong(userData?.songs[0].id);
+  } else {
+    const currentSongIndex = getCurrentSongIndex();
+    const nextSong = userData?.songs[currentSongIndex + 1];
+
+    playSong(nextSong.id);
+  }
+};
+
 
 
 const renderSongs = (array) => {
